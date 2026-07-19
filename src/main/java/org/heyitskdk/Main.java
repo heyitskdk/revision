22ca6f136f7afaa5a1af253ef480aac1279f0b49
heyitskdk <keshav.duttkaushik23@gmail.com>
2026-07-19 07:54:31 +0530

    first commit

---
+++ src/main/java/org/heyitskdk/Main.java
@@ -0,0 +1,11 @@
+package org.heyitskdk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String out = """
+                Karma is the only escape.
+                """;
+        System.out.print(out);
+    }
+}
